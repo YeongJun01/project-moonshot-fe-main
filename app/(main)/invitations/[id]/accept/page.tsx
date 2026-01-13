9728612c097bfbbc3c667c0d67c5c4a4d700b015
Yeongjun <ing08114@gmail.com>
2026-01-13 13:18:05 +0900

    초대 수정

--- app/(main)/invitations/[id]/accept/page.tsx
+++ app/(main)/invitations/[id]/accept/page.tsx
@@ -1,5 +1,5 @@
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function InvitationAcceptPage({
   params,
@@ -9,25 +9,23 @@ export default async function InvitationAcceptPage({
   const { id } = await params;
 
   const cookieStore = await cookies();
-  const token = cookieStore.get('access-token')?.value;
-
-  if (!token) redirect('/login');
+  const token = cookieStore.get("access-token")?.value;
+  console.log(token);
+  if (!token) redirect("/login");
 
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/invitations/${id}/accept`,
     {
-      method: 'POST',
+      method: "POST",
       headers: {
         Authorization: `Bearer ${token}`,
       },
-      cache: 'no-store',
+      cache: "no-store",
     }
   );
-
   if (!res.ok) {
-    redirect('/projects');
+    redirect("/projects");
   }
-
   const { projectId } = (await res.json()) as { projectId: number };
 
   console.log(`프로젝트아이디: ${projectId}`);
